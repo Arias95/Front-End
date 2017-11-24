bd9af464699a62b1b47590e8441713a6ab3e1afc
Andres Arias <andres.arias12@gmail.com>
2017-11-23 20:04:58 -0600

    Componente publicacion con data binding

--- src/app/publication/publication.component.ts
+++ src/app/publication/publication.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-publication',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publication.component.css']
 })
 export class PublicationComponent implements OnInit {
+  @Input() title: string;
+  @Input() user: string;
+  @Input() desc: string;
+  @Input() skill: string;
+  private visible: boolean;
 
   constructor() { }
 
