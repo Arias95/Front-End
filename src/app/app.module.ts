8fa15f4821754b6e8c462f9ac917640c3407ec31
Andres Arias <andres.arias12@gmail.com>
2017-11-26 11:06:12 -0600

    Modularizado mas el asunto con NgModule. Para que no digan que soy un cochino

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { YoutubePlayerModule } from 'ngx-youtube-player';
+import { PublicationModule } from '../publication/publication.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
-import { PublicationComponent } from './publication/publication.component';
-import { VideoComponent } from './video/video.component';
-import { FileComponent } from './file/file.component';
-import { GithubComponent } from './github/github.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProfileComponent,
-    PublicationComponent,
-    VideoComponent,
-    FileComponent,
-    GithubComponent
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    YoutubePlayerModule
+    PublicationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/publication/publication.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { YoutubePlayerModule } from 'ngx-youtube-player';
+
+import { PublicationComponent } from './publication/publication.component';
+import { VideoComponent } from './video/video.component';
+import { FileComponent } from './file/file.component';
+import { GithubComponent } from './github/github.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserAnimationsModule,
+        YoutubePlayerModule
+    ],
+    declarations: [
+        PublicationComponent,
+        VideoComponent,
+        FileComponent,
+        GithubComponent
+    ],
+    providers: [],
+    exports: [
+        PublicationComponent,
+        VideoComponent,
+        FileComponent,
+        GithubComponent
+    ]
+})
+
+export class PublicationModule {}
